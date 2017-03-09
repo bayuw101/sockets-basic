39f32fe32761ea4894911d2b92f72545a707b5db
Bayu <emailbayuwicaksono@gmail.com>
2017-03-09 11:13:06 +0700

    add custom

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,12 @@ var socket = io();
 socket.on('connect', function(){
 	console.log('user connected to socket.io');
 });
+
+socket.on('message', function(message){
+	console.log('New Message !');
+	console.log(message.text);
+});
+
+socket.emit('message', {
+	text : 'Hay bro !'
+});
--- server.js
+++ server.js
@@ -4,8 +4,15 @@ var http = require('http').Server(app);
 var PORT = process.env.PORT || 3000;
 var io = require('socket.io')(http);
 
-io.on('connection', function(){
+io.on('connection', function(socket){
 	console.log("You're connected with socket.io");
+	socket.on('message', function(message){
+		console.log("Message received : " + message.text);
+		socket.broadcast.emit('message', message);
+	});
+	socket.emit('message', {
+		text : 'Welcome to chat room !'
+	});
 });
 
 app.use(express.static(__dirname + '/public'));
