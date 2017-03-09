fa771c1762f4b8bd13bdde896b8d07e2802c8421
Bayu <emailbayuwicaksono@gmail.com>
2017-03-09 11:39:55 +0700

    add front end form to submit

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,21 @@
 var socket = io();
 
-socket.on('connect', function(){
+socket.on('connect', function() {
 	console.log('user connected to socket.io');
 });
 
-socket.on('message', function(message){
+socket.on('message', function(message) {
 	console.log('New Message !');
 	console.log(message.text);
 });
 
-socket.emit('message', {
-	text : 'Hay bro !'
+var $form = $('#message-form');
+
+$form.on('submit', function(event) {
+	$message = $form.find('input[name=message]');
+	event.preventDefault();
+	socket.emit('message', {
+		text: $message.val()
+	});
+	$message.val('')
 });
